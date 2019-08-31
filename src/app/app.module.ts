1faa3c0489ea8dd96dc0c803b9913b27bd414d06
fancn <fanyj@yikeer.com>
2019-08-31 18:03:01 +0800

    remove uncessary imports when lazy load applied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeModule } from "./home/home.module";
-import { ContactsModule } from "./contacts/contacts.module";
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, ContactsModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
