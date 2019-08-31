7063945da2a574724f08bcc518f64ba6d90e1238
fancn <fanyj@yikeer.com>
2019-08-31 13:49:40 +0800

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { ContactsComponent } from "./contacts/contacts.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent
+    loadChildren: () => import("./home/home.module").then(mod => mod.HomeModule)
   },
   {
     path: "contacts",
-    component: ContactsComponent
+    loadChildren: () =>
+      import("./contacts/contacts.module").then(mod => mod.ContactsModule)
   },
   {
     path: "**",
